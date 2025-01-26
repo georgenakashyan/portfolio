f5710545a08b87bdddd2e0b828dd59461dee5329
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-01-26 15:37:04 -0500

    style: add custom theme to global css and ts config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				primary_dark: "var(--gn-primary-dark)",
+				primary: "var(--gn-primary)",
+				secondary: "var(--gn-secondary)",
+				secondary_light: "var(--gn-secondary-light)",
+				link: "var(--gn-link)",
+				text_primary: "var(--gn-text-primary)",
+				text_secondary: "var(--gn-text-secondary)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
