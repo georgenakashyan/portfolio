c5ea5910ce1c4c8cada07dc6d1e70d876b2c9bee
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-02-03 23:51:51 -0500

    bug: add location, fixing build time error

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -16,14 +16,15 @@ const Experience = ({
 	description,
 }: ExperienceProps) => {
 	return (
-		<div className='grid grid-cols-10 grid-rows-1 space-x-10 pb-4'>
+		<div className='grid grid-cols-10 grid-rows-1 -m-6 p-6 space-x-10 border border-background rounded-3xl hover:bg-opacity-5 hover:bg-slate-200 transition'>
 			<div className='col-span-3 text-sm pt-1 min-w-fit text-text_secondary'>
 				{startDate} — {endDate}
 			</div>
 			<div className='col-span-7'>
-				<p className='text-lg font-semibold'>{position}</p>
-				<p className='text-md'>{company}</p>
-				<ul className='list-disc pl-5 pt-2'>
+				<p className='text-xl font-semibold'>{position}</p>
+				<p className='text-lg'>{company}</p>
+				<p className='text-md'>{location}</p>
+				<ul className='list-disc pl-5 pt-2 text-text_secondary'>
 					{description.map((bullet, index) => (
 						<li
 							key={index}
